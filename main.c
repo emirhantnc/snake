e8d72d9d4f3cf4035417a1a6e48e050497bf9062
emirhantnc <e.tunc3519@gmail.com>
2026-02-11 18:02:53 +0100

    nouveau

--- main.c
+++ main.c
@@ -1,8 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
-int main(void){
+struct cell_s{
+    int val;
+    struct cell_s *next;
+};
+typedef struct cell_s cell;
+
+struct queue_s{
+    cell *head;
+    cell *tail;
+};
+typedef struct queue_s queue;
+
+queue 
+*snake(void){
+    queue *q = malloc(sizeof(queue));
+    q->head = NULL;
+    q->tail = NULL;
+    return q;
+}
+
+void 
+add (queue *q, int x){
+
+    cell *new = malloc(sizeof(cell));
+    new->val = x;
+    new->next = NULL;
+
+    if(q->head == NULL){
+        q->head = new;
+    } else {
+        q->tail->next = new;
+        
+    }
+    q->tail = new;
+}
+
+
+void 
+drawMap(int n, int p){
     
-    printf("hello world");
+    for(int i = 0 ; i<n; i++ ){
+        for(int j = 0 ; j<p ; j++){
 
-    return 0;
+            if(i== 0|| i== n-1 || j == 0 || j == p-1){
+                printf("#");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int 
+main(void){
+    bool game_running = true;
+    
+    int l = 10 ,p = 20;
+
+    drawMap(l,p);
+    
+    
+    
 }
